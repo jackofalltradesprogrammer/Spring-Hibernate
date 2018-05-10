805d6c51bd66e9a0a64f73cfe13f01ca0910214d
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-05-10 14:24:54 -0400

    Configure the class for setter dependency

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -1,6 +1,19 @@
 package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// create a no-arg constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+	
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method setFortuneService");
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -10,7 +23,7 @@ public class CricketCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
